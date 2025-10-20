48cda92dcd358604bce53bd98c65a7f91507bbac
Ilyasse <84371108+itaame@users.noreply.github.com>
2025-10-20 05:03:37 +0200

    Restore GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/PortFolio/',
+  base: mode === 'production' ? '/PortFolio/' : '/',
   plugins: [react()],
   build: {
     sourcemap: true
